987ddd4f770a3024d6c599782192dd453b99ca48
czaro <cezarykalinowski05@gmail.com>
2022-12-06 18:33:07 +0100

    add ui of addCardFragment

--- app/src/main/java/com/example/subscriptionmanager/SecondFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.subscriptionmanager.databinding.FragmentSecondBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,6 +15,7 @@ import com.example.subscriptionmanager.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,15 +27,29 @@ class SecondFragment : Fragment() {
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+
+
+
         return binding.root
+    }
+
+    private fun showDatePicker(){
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+        datePicker.show(parentFragmentManager, "DP")
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.datePickerBtn.setOnClickListener(){
+            showDatePicker()
         }
     }
 
