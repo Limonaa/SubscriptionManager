38cddc82fb41acfd03a61c18947a33a9518fc322
limona <mateusz.walaszek@op.pl>
2022-12-18 15:47:38 +0100

    person recyclerView adapter

--- app/src/main/java/com/example/subscriptionmanager/fragments/ShowDetailsFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/fragments/ShowDetailsFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.subscriptionmanager.R
+import com.example.subscriptionmanager.adapters.PersonAdapter
+import com.example.subscriptionmanager.data.Person
 import com.example.subscriptionmanager.databinding.FragmentShowDetailsBinding
 
 class ShowDetailsFragment : Fragment() {
@@ -25,6 +29,15 @@ class ShowDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        val peopleList = mutableListOf(
+            Person("Jacek Marko", "22 września", "22PLN", R.drawable.avatar1),
+            Person("Maja Kwiatczyńska", "17 grudnia", "7PLN", R.drawable.avatar2),
+            Person("Maciej Kuźdup", "28 lutego", "112PLN", R.drawable.avatar3)
+        )
+        val adapter = PersonAdapter(peopleList)
+        binding.rvPeople.adapter = adapter
+        binding.rvPeople.layoutManager = LinearLayoutManager(requireContext())
     }
 
     override fun onDestroyView() {
