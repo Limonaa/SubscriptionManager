2881f81b19d577181ee1ae242b54ac527d2508b5
Limona <mateusz.walaszek@op.pl>
2023-01-04 22:53:28 +0100

    add constants value

--- app/src/main/java/com/example/subscriptionmanager/fragments/AddSubFragment.kt
+++ app/src/main/java/com/example/subscriptionmanager/fragments/AddSubFragment.kt
@@ -4,23 +4,21 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.subscriptionmanager.R
 import com.example.subscriptionmanager.data.Subscription
 import com.example.subscriptionmanager.databinding.FragmentAddSubBinding
-import com.example.subscriptionmanager.other.Constans
+import com.example.subscriptionmanager.other.Constants
 import com.example.subscriptionmanager.viewmodels.MainViewModel
 import com.google.android.material.chip.Chip
 import java.util.*
@@ -29,7 +27,6 @@ class AddSubFragment : Fragment() {
 
     private var _binding: FragmentAddSubBinding? = null
     private val binding get() = _binding!!
-
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -69,12 +66,12 @@ class AddSubFragment : Fragment() {
 
     private fun setupChips() {
 
-        for (item in Constans.COMPANIES) {
+        for (item in Constants.COMPANIES) {
             val chip = Chip(requireContext(), null, com.google.android.material.R.style.Widget_Material3_Chip_Suggestion)
             chip.apply {
                 id = ViewCompat.generateViewId()
                 text = item.name
-                //TODO set chip icon resource (in Constans.COMPANIES)
+                //TODO set chip icon resource (in Constants.COMPANIES)
                 setChipIconResource(item.image)
                 setOnClickListener {
                     binding.tiName.setText(chip.text)
@@ -87,14 +84,15 @@ class AddSubFragment : Fragment() {
 
     private fun saveNewSubscription() {
 
+        //TODO check if ivImage is equal to drawable insert_image
+        //TODO then replace image with first letter of sub name
+
         viewModel.addSubscription(Subscription(
             ViewCompat.generateViewId(),
             binding.tiName.text.toString(),
             binding.tiDate.text.toString(),
             binding.tiPrice.text.toString(),
-            ContextCompat.getDrawable(requireContext(), R.drawable.netflix_icon_161073)?.toBitmap()
-        // TODO replace drawable with selected photo
-        // TODO OR if empty replace drawable with image of subscription's first letter
+            binding.ivImage.drawable.toBitmap()
             )
         )
         findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
@@ -117,14 +115,14 @@ class AddSubFragment : Fragment() {
 
         val intent = Intent(Intent(Intent.ACTION_PICK))
         intent.type = "image/*"
-        startActivityForResult(intent, 100)
-        //TODO select photo from gallery
+        startActivityForResult(intent, Constants.IMAGE_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+        //IMAGE FOR SUBSCRIPTION
+        if (requestCode == Constants.IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             binding.ivImage.setImageURI(data?.data)
             binding.ivImage.setColorFilter(Color.argb(0, 0, 0, 0))
         }
